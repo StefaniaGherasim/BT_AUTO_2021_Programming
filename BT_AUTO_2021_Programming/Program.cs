b6f7bf865079e3168ef1e76ce10d34a19d88407c
Stefania Gherasim <sstefaniadaniela@gmail.com>
2021-12-15 10:35:02 +0200

    methode

--- BT_AUTO_2021_Programming/Program.cs
+++ BT_AUTO_2021_Programming/Program.cs
@@ -6,10 +6,12 @@ namespace BT_AUTO_2021_Programming
     {
         static void Main(string[] args)
         {
-            Course01(args);
-           // Course02(args);
+          //Course01(args);
+          //Course02(args);
+          Course03(args);
 
         }
+
         static void Course01(string[] args)
         {
             const int MAX_SIZE = 100;
@@ -304,6 +306,83 @@ namespace BT_AUTO_2021_Programming
 
 
         }
+
+    static void Course03(string[] args)
+        {
+            //ComputeConversion(args);
+            CurrencyCalculator(156, "USD", 4.3734);
+            Shape s = GetShape(12, 34, 12.4, 'a', "17d");
+            //Console.WriteLine(ConcatenateStrings(args));
+            Random rnd = new Random();
+            GuessNumber(rnd.Next(1, 1000));
+        }
+
+        static bool IsOddNumber(int number)
+        {
+             return number % 2 != 0;
+        }
+
+        static int TestValue(int testval, int target)
+        { 
+            if (testval < target)
+            {
+                Console.WriteLine("Number to be guess is higher!");
+                return -1;
+            }
+            if (testval > target)
+            {
+                Console.WriteLine("Number to be guess is lower!");
+                return 1;
+            }
+            return 0;
+        }
+
+        static void GuessNumber (int n)
+        { int currentNumber = 0;
+            do
+            {
+                currentNumber = int.Parse(Console.ReadLine());
+                Console.WriteLine("Number chosen is {0}", currentNumber);
+            }
+            while (TestValue(currentNumber, n) != 0);
+        }
+
+
+        static Shape GetShape(int x, int y, double a, char t, string s)
+        {
+            Console.WriteLine(x);
+            return new Shape();
+        }
+
+        static string ConcatenateStrings(string[] args)
+        {
+            string returnString = "";
+
+            foreach (string s in args)
+            {
+                returnString += s; // returnString = returnString + s;
+            }
+            return returnString;
+        }
+
+           static public void ComputeConversion(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("The aplication receives only 3 parameters: amount, currency, exchange rate");
+            }
+            else
+            {
+                Console.WriteLine("{0} {1} {2}", args[0], args[1], args[2]);
+                CurrencyCalculator(Double.Parse(args[0]), args[1], Double.Parse(args[2]));
+            }
+        }
+
+        static void CurrencyCalculator(double amount, string currency, double conversionRate)//semnatura functiei
+        {
+            Console.WriteLine("By converting {0} {1} into RON we obtain {2} RON", amount, currency, amount * conversionRate);
+        }
     }
+
 }   
 
