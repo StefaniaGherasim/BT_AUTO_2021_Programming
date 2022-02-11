b4896349ffc959209c4e8abb44b82feb782cef8e
StefaniaGherasim <sstefaniadaniela@gmail.com>
2022-02-11 10:44:50 +0200

    Excel, XML, Json, AllFilesInFolder,

--- NUnit_Auto_2022/Tests/AuthenticationSite/AuthTest.cs
+++ NUnit_Auto_2022/Tests/AuthenticationSite/AuthTest.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Linq;
+using System.Xml.Serialization;
 
 namespace NUnit_Auto_2022.Tests
 {
@@ -52,9 +53,48 @@ namespace NUnit_Auto_2022.Tests
             }
         }
 
-        // test authentication with page object model (pom)
-        [Test, TestCaseSource("GetCredentialsData")]
+        private static IEnumerable<TestCaseData> GetCredentialsDataCSV3()
+        {
+            var csvData = Utils.GetDataTableFromCsv("TestData\\credentials.csv");
+                for(int i=0; i< csvData.Rows.Count; i++)
+            {
+                yield return new TestCaseData(csvData.Rows[i].ItemArray);
+            }
+        }
 
+        private static IEnumerable<TestCaseData> GetCredentialsDataExcel()
+        {
+            var excelData = Utils.GetDataTableFromExcel("TestData\\credentials.xlsx");
+            for(int i = 0; i < excelData.Rows.Count; i++)
+            {
+                yield return new TestCaseData(excelData.Rows[i].ItemArray);
+            }
+        }
+
+        private static IEnumerable<TestCaseData> GetCredentialsDataJson()
+        {
+           // DataModels.Credentials credentials = Utils.JsonRead<DataModels.Credentials>("TestData\\crededntials.json");
+            var credentials = Utils.JsonRead<DataModels.Credentials>("TestData\\crededntials.json");
+            yield return new TestCaseData(credentials.Username, credentials.Password);
+        }
+
+        private static IEnumerable<TestCaseData> GetCredentialsDataXml()
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(DataModels.Credentials));
+            foreach (var file in Utils.GetAllFilesInFolderExt("TestData\\", "*.xml"))
+            {
+                Console.WriteLine("Testing with file:" + file);
+                using (Stream reader = new FileStream("TestData\\Credentials.xml", FileMode.Open))
+                {
+                    var credentials = (DataModels.Credentials)serializer.Deserialize(reader);
+                    yield return new TestCaseData(credentials.Username, credentials.Password);
+                }
+            }
+
+        }
+
+        // test authentication with page object model (pom)
+        [Test, TestCaseSource("GetCredentialsDataXml")]
         public void BasicAuth(string username, string password)//contine instantieri de pagini, apleluri catre paginile respective si aserturi 
         {
             // driver.Navigate().GoToUrl("http://86.121.249.150:4999/#/login");//navigam pe pagina
